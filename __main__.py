7aeea8422f84997b601f103deb3ad5b90a12c0a0
daridev <darideveloper@gmail.com>
2023-08-17 15:57:22 -0600

    feat (main): run viwers bot

--- __main__.py
+++ __main__.py
@@ -1,7 +1,4 @@
 from cookies_getter import CookiesGetter
 
-cookies_getter = CookiesGetter ("botviews")
-cookies_getter.auto_run ()
-
-cookies_getter = CookiesGetter ("botcheers")
+cookies_getter = CookiesGetter ("viwers")
 cookies_getter.auto_run ()
